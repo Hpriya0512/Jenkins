027cc05c6497a4ef242e72f8499cd2c9bc5b1fe3
Hpriya0512 <121761700+Hpriya0512@users.noreply.github.com>
2023-02-09 12:02:56 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,15 +3,8 @@ pipeline {
     stages {
         stage('build') {
             steps {
-                
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
-
-            }
+                echo "Hello World!"
+                }
         }
     }
 }
-
-
-
